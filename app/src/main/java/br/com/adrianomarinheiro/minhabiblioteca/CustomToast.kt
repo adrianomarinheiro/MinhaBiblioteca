efb6312bb36a1fa085e10d8f186a8e0f98e8de1c
Adriano Marinheiro <adrianomarinheiro@gmail.com>
2019-04-03 22:21:51 -0300

    Adição de Toast customizado

---
+++ app/src/main/java/br/com/adrianomarinheiro/minhabiblioteca/CustomToast.kt
@@ -0,0 +1,29 @@
+package br.com.adrianomarinheiro.minhabiblioteca
+
+import android.app.Activity
+import android.view.Gravity
+import android.widget.Toast
+import kotlinx.android.synthetic.main.custom_toast.view.*
+
+
+class CustomToast {
+
+    companion object {
+
+        fun show(activity: Activity, mensagem: String) {
+            val inflater = activity.layoutInflater
+
+            val customToastroot = inflater.inflate(R.layout.custom_toast, null)
+
+            val customtoast = Toast(activity)
+
+            customToastroot.textView1.text = mensagem
+
+            customtoast.view = customToastroot
+            customtoast.setGravity(Gravity.CENTER_HORIZONTAL or Gravity.BOTTOM, 0, 0)
+            customtoast.duration = Toast.LENGTH_LONG
+            customtoast.show()
+        }
+
+    }
+}
