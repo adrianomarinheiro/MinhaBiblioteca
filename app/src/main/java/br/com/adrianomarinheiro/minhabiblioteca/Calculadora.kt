caea65ff06d7be5cdad72fa13dba19fb52655ca7
Logon Aluno <logonrmlocal@fiap.com.br>
2019-04-03 21:01:56 -0300

    Initial commit

---
+++ app/src/main/java/br/com/adrianomarinheiro/minhabiblioteca/Calculadora.kt
@@ -0,0 +1,15 @@
+package br.com.adrianomarinheiro.minhabiblioteca
+
+class Calculadora {
+
+    fun somar(v1: Int, v2: Int): Int {
+        return v1 + v2
+    }
+
+    fun subtrair(v1: Int, v2: Int): Int {
+        return v1 - v2
+    }
+
+
+
+}
